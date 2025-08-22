2d6128ef6e43cfb477aefc326a4b30ea5ac1a3b9
Faraz Hassan Naqvi <faraznaqvi729927@gmail.com>
2025-08-22 19:28:48 +0500

    Updated the project

--- src/App.jsx
+++ src/App.jsx
@@ -373,7 +373,7 @@ export default function Portfolio() {
 
             </div>
 
-            <p className={`text-xl md:text-2xl text-gray-300 mb-12 max-w-3xl mx-auto leading-relaxed transition-all duration-1000 delay-600 ${isLoaded ? 'translate-y-0 opacity-100' : 'translate-y-16 opacity-0'
+            <p className={`text-xl md:text-2xl text-gray-300 mb-12 max-w-8xl mx-auto leading-relaxed transition-all duration-1000 delay-600 ${isLoaded ? 'translate-y-0 opacity-100' : 'translate-y-16 opacity-0'
               }`}>
               Crafting extraordinary digital experiences with React and modern UI tools —
               building responsive, user-friendly interfaces and mastering frontend development best practices.
